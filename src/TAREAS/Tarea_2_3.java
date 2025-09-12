c2e19b0a351f36073ed9c1c8eacb16ce10ae67f1
pabloforteaecheverria-pixel <pablo.fortea.echeverria@gmail.com>
2025-09-12 09:49:20 +0200

    ejercicio 2_3

---
+++ src/TAREAS/Tarea_2_3.java
@@ -0,0 +1,31 @@
+package TAREAS;
+
+import java.util.Scanner;
+
+public class Tarea_2_3 {
+
+	public static void main(String[] args) {
+		int millares;
+		int centenas;
+		int decenas;
+		int unidades;
+		
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Ingresa un numero de 4 cifras: ");
+		int numero = sc.nextInt();
+		
+		millares = numero/1000;
+		centenas = (numero % 1000)/100;
+		decenas = (( numero  % 1000) % 100)/10;
+		unidades = ((( numero  % 1000) % 100)%10);
+		
+		System.out.printf("Millares: %d%n", millares);
+		System.out.printf("Centenas: %d%n", centenas);
+		System.out.printf("Decenas: %d%n", decenas);
+		System.out.printf("Unidades: %d", unidades);
+
+		sc.close();
+	}
+
+}
